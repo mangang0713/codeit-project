c6bb6af60cf825390dbda278464a9b672a36def2
cgwdesktop <jhl011205@gmail.com>
2024-01-20 17:56:45 +0900

    email password checking system, make folder file

--- signin.js
+++ signin.js
@@ -1,22 +1,45 @@
-const myInput = document.querySelector('.sign-input');
-const errorMessage = document.getElementById('error-message');
+const myInputEmail = document.querySelector('.input-email');
+const myInputPassword = document.querySelector('.input-password');
+const errorMessageId = document.getElementById('error-message-id');
+const errorMessagePw = document.getElementById('error-message-pw');
+const login = document.querySelector('.cta');
+const answerEmail = 'test@codeit.com';
+const answerPassword = 'codeit101';
 
 function isValidEmail(email) {
   let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
 
-
-myInput.addEventListener('focusout', function() {
-  if (myInput.value.trim() === '') {
-    errorMessage.textContent = '아래에 이메일 주소를 입력해 주세요.';
-    errorMessage.style.display = 'block';
-  } else if (!isValidEmail(myInput.value.trim())) {
-    errorMessage.textContent = '올바른 이메일 주소가 아닙니다.';
-    errorMessage.style.display = 'block';
+myInputEmail.addEventListener('focusout', function() {
+  if (myInputEmail.value.trim() === '') {
+    errorMessageId.textContent = '이메일 주소를 입력해 주세요.';
+    errorMessageId.style.display = 'block';
+  } else if (!isValidEmail(myInputEmail.value.trim())) {
+    errorMessageId.textContent = '올바른 이메일 주소가 아닙니다.';
+    errorMessageId.style.display = 'block';
   } else {
-    errorMessage.style.display = 'none';
+    errorMessageId.style.display = 'none';
   }
 });
 
+myInputPassword.addEventListener('focusout', function() {
+  if (myInputPassword.value.trim() === '') {
+    errorMessagePw.textContent = '비밀번호를 입력해 주세요.';
+    errorMessagePw.style.display = 'block';
+  } else {
+    errorMessagePw.style.display = 'none';
+  }
+});
+
+login.addEventListener('click', function() { 
+  if (myInputEmail.value === answerEmail && myInputPassword.value == answerPassword) {
+    window.location.href = '/folder';
+  } else {
+    errorMessageId.textContent = '이메일을 확인해 주세요.';
+    errorMessageId.style.display = 'block';
+    errorMessagePw.textContent = '비밀번호를 확인해 주세요.';
+    errorMessagePw.style.display = 'block';
+  }
+});
 
